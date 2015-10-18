a94c7897625bcc7941b665ab330e2f76a3978b4f
stanleycyang <stanley@stanleycyang.com>
2015-10-17 18:38:16 -0700

    Created dev server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,35 @@
 'use strict'
 
 const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const port = require('../config').port + 1
 
-module.exports = {
-  devtool: 'eval-source-map',
+
+const config = module.exports = {
   entry: {
-    'webpack-hot-middleware/client?reload=true'
+
   },
   output: {
     path: path.resolve('build'),
-    filename: '[name].js',
-    publicPath: '/'
+    publicPath: 'http://localhost:' + port + '/',
+    filename: '[name].js'
   },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ]
+  },
   module: {
     loaders: [
-      // babel + jax loader
+      // babel + jsx loader
       {
-        test: /\.js?$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: [
+        loaders: [
+          'react-hot',
           'babel'
         ]
       },
@@ -45,21 +40,38 @@ module.exports = {
           'json'
         ]
       },
+      // css loader
       {
-        test:/\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css',
+          'postcss'
+        ]
       }
+    ],
+    plugins: [
+      new webpack.NoErrorsPlugin()
     ]
   },
   postcss: function () {
     return [
       require('postcss-import'),
-      require('postcss-cssnext'),
-      require('cssnano')({
-        safe: true
-      })
+      require('postcss-cssnext')
     ]
   },
-  // node node built-ins
   node: false
 }
+
+const DevServer = new WebpackDevServer(webpack(config), {
+  contentBase: path.resolve('build') + '/',
+  hot: true,
+  quiet: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
+})
+
+DevServer.listen(port, 'localhost', (err) => {
+  if (err) throw err
+})
---
+++ client/webpack.production.config.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+}
