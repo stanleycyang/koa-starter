c2c1b92eecc05ae434c8c93d530fe2349ca5315a
stanleycyang <stanley@stanleycyang.com>
2015-10-17 18:53:54 -0700

    Added a base component

---
+++ client/components/main/index.js
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+index.propTypes = {}
+index.defaultProps = {}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,13 +5,18 @@ const WebpackDevServer = require('webpack-dev-server')
 const path = require('path')
 const port = require('../config').port + 1
 
+const entrypoint_prefix = [
+  'webpack/hot/only-dev-server',
+  'webpack-dev-server/client?http://localhost:' + port
+]
 
 const config = module.exports = {
   entry: {
-
+    // Root component. Add additional components with the same prefix, ie. entrypoint_prefix.concat('your_entry_point')
+    'main': entrypoint_prefix.concat('./client/components/main/index')
   },
   output: {
-    path: path.resolve('build'),
+    path: path.resolve('client', 'build'),
     publicPath: 'http://localhost:' + port + '/',
     filename: '[name].js'
   },
